d7854411dca6220e41e2133c08623a80c27e1864
baijiayong <jiayongbai@gmail.com>
2014-08-01 13:10:57 +0800

    打印出测试结果

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -5,6 +5,6 @@ public class FizzBuzzTest
     public static void main(String args[])
     {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        System.out.println(fizzBuzz.say(1));
+        System.out.println("1".equals(fizzBuzz.say(1)));
     }
 }
