5397af8e8656fbb43606e09f6c8b5c9c65050706
baijiayong <jiayongbai@gmail.com>
2014-08-01 13:01:35 +0800

    添加了测试1,打印出1

---
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -0,0 +1,9 @@
+package com.baldurtech;
+
+public class FizzBuzz
+{
+    public String say(Integer num)
+    {
+        return "1";
+    } 
+}
---
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package com.baldurtech;
+
+public class FizzBuzzTest
+{
+    public static void main(String args[])
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.say(1));
+    }
+}
