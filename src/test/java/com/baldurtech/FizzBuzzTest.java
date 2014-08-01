2fdbdcdc2da32b62e92534e58cd29c976a05896d
baijiayong <jiayongbai@gmail.com>
2014-08-01 15:53:40 +0800

    将结果进行判断，然后直接输出最终结果

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -5,7 +5,23 @@ public class FizzBuzzTest
     public static void main(String args[])
     {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        System.out.println("1".equals(fizzBuzz.say(1)));
-        System.out.println("2".equals(fizzBuzz.say(2)));
+        Boolean testResult = true;
+        if( ! "1".equals(fizzBuzz.say(1)))
+        {
+            testResult = false;
+            System.out.println("Expected 1 But " + fizzBuzz.say(1) );
+        }
+        if( ! "2".equals(fizzBuzz.say(2)))
+        {
+            testResult = false;
+            System.out.println("Expected 2 But " + fizzBuzz.say(2) );
+        }
+        if(testResult)
+        {
+           System.out.println("Test Success!"); 
+        }else
+        {
+            System.out.println("Test Failed!");
+        }
     }
 }
