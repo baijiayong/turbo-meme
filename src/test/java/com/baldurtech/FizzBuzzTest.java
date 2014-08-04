2ca2f28ae9eb83a6f99ddd02529c173dbcf76b54
baijiayong <jiayongbai@gmail.com>
2014-08-04 10:40:38 +0800

    把测试案例提成方法

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -7,17 +7,24 @@ public class FizzBuzzTest
     public static void main(String args[])
     {
         String  actualResult = null;
-        
+        outputTestReport(testResult);
+    }
+    public static void test_1_should_be_1()
+    {
         FizzBuzz fizzBuzz1 = new FizzBuzz();
         assertEquals("1",fizzBuzz1.say(1));
+    }
        
+    public static void test_2_should_be_2()
+    {
         FizzBuzz fizzBuzz2 = new FizzBuzz();
         assertEquals("2",fizzBuzz2.say(2));
-       
+    }
+         
+    public static void test_3_should_be_Fizz()
+    {
         FizzBuzz fizzBuzz3 = new FizzBuzz();
         assertEquals("Fizz",fizzBuzz3.say(3));
-        
-        outputTestReport(testResult);
     }
     public static void outputTestReport(Boolean testResult)
     {
@@ -35,7 +42,6 @@ public class FizzBuzzTest
         {
             testResult = false;
             System.out.println("Expected " + expectedResult + " But " + actualResult );
-        }
-        
+        } 
     }
 }
